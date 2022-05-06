cd5fdb5b9f90847c03edfd94b278ea2aca229774
ushieru kokoran <uzielcocolan@gmail.com>
2022-05-05 23:12:29 -0500

    User $PORT when is available

--- bin/server.dart
+++ bin/server.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
 void main() async {
+  var port = int.tryParse(Platform.environment['PORT'] ?? '') ?? 8080;
   var server = await shelf_io.serve(
-      (Request request) => Response.ok('Hello World!'), '0.0.0.0', 8080);
+      (Request request) => Response.ok('Hello World!'), '0.0.0.0', port);
 
   print('Serving at http://${server.address.host}:${server.port}');
 }
